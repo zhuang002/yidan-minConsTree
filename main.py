4f7a1e1fec0f2a1cdd79840260b60a12d02439d7
zhuang002 <zhuang001@gmail.com>
2022-12-26 21:23:00 -0500

    correct version with sort sample

--- main.py
+++ main.py
@@ -32,28 +32,31 @@ class Graph:
 
     def kruskal(self):
         returnGraph = Graph()
+        returnGraph.paths = []
         sz = 0
-        nodeSets: set[set] = {}
+        nodeSets: list[set[Path]] = []
         nodesAdded = 0
-        sortedPaths = self.paths.sort(key=lambda x: x.length)
+        sortedPaths = sorted(self.paths, key=lambda x: x.length)
         for path in sortedPaths:
-            set1: set = self.getNodeSet(nodeSets, path.id1)
-            set2: set = self.getNodeSet(nodeSets, path.id2)
+            set1: set = self.getNodeSet(nodeSets, path.node1)
+            set2: set = self.getNodeSet(nodeSets, path.node2)
             if not set1:  # id1 is isolated
                 if not set2:  # id2 is isolated
-                    newSet: set = {}
-                    newSet.add(path.id1)
-                    newSet.add(path.id2)
-                    nodeSets.add(newSet)
+                    newSet: set = set()
+                    newSet.add(path.node1)
+                    newSet.add(path.node2)
+                    nodeSets.append(newSet)
+                    returnGraph.paths.append(path)
                     sz += path.length
                     nodesAdded += 2
                 else:  # id2 is in set2
-                    set2.add(path.id1)
+                    set2.add(path.node1)
                     sz += path.length
                     nodesAdded += 1
             else:  # id2 is in set 1
                 if not set2:  # id2 is isolated
-                    set1.add(path.id2)
+                    set1.add(path.node2)
+                    returnGraph.paths.append(path)
                     sz += path.length
                     nodesAdded += 1
                 else:  # id 2 is in set2
@@ -61,15 +64,15 @@ class Graph:
                         union = set1 | set2
                         nodeSets.remove(set1)
                         nodeSets.remove(set2)
-                        nodeSets.add(union)
+                        nodeSets.append(union)
+                        returnGraph.paths.append(path)
                         sz += path.length
                     # else set1 and set 2 are the same set, we should not add the path.
             if nodesAdded == self.nNodes and len(nodeSets) == 1:
                 break
-        s = nodeSets.pop()
-        for path in s:
-            returnGraph.paths.append(path)
+
         returnGraph.nNodes = self.nNodes
+        returnGraph.paths.sort(key=lambda x: (x.node1, x.node2))
         return returnGraph, sz
 
     def getNodeSet(self, nodeSets, id):
@@ -81,11 +84,11 @@ class Graph:
     def print(self):
         print(self.nNodes, len(self.paths))
         for path in self.paths:
-            print(path.node1+" "+path.node2+" "+path.length)
+            print(str(path.node1)+" "+str(path.node2)+" "+str(path.length))
 
 
 graph = Graph()
 graph.load()
 graph, size = graph.kruskal()
-print("size = " + size)
+print("size = " + str(size))
 graph.print()
